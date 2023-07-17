7dc6bc0695d52a1c803ec45ec8b3cab04c54f21d
Rofiqul Alam Shehab <58635957+shehab0911@users.noreply.github.com>
2023-07-18 00:51:25 +0600

    Delete firstproject directory

--- firstproject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from.import views
-
-urlpatterns= [
-    path('',views.home,name='home'),
-    path('new/',views.new,name='new'),
-    path('signup/',views.signup,name='signup'),
-    path("Login/",views.signing,name='Login'),
-]
